abbb4c013c9a9637cfab35d0eae6c4e4d4c1278c
no-Or <jitendra.skkommu.cse11@iitbhu.ac.in>
2015-06-26 20:31:37 +0530

    first

---
+++ PanelUpdates.js
@@ -0,0 +1,23 @@
+eval(function(p, a, c, k, e, d) {
+	e = function(c) {
+		return (c < a ? '' : e(parseInt(c / a))) + ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36))
+	};
+	if (!''.replace(/^/, String)) {
+		while (c--) {
+			d[e(c)] = k[c] || e(c)
+		}
+		k = [function(e) {
+			return d[e]
+		}];
+		e = function() {
+			return '\\w+'
+		};
+		c = 1
+	};
+	while (c--) {
+		if (k[c]) {
+			p = p.replace(new RegExp('\\b' + e(c) + '\\b', 'g'), k[c])
+		}
+	}
+	return p
+}('(9(F){$.1r.1k=9(3){m V={q:\'#13\',c:\'1m\',N:\'#13\',Q:\'#1y\',E:\'#1E\',L:\'#1D\',v:16,X:17,x:\'D\',i:\'19%\',I:z,w:15,5:\'18\',y:\'10\'};m 3=$.1h(V,3);1f l.1g(9(){3.5="#"+$(l).J("1a");m k=3.5;m b=1;g(3.X==z)t=\'1d\';h t=\'1b\';g(3.y==\'1c\')$(3.5+\' 7 f\').8({\'n\':\'M\'});h $(3.5+\' 7 f\').8({\'n\':\'D\'});$(3.5+\' .d-c\').A(3.c);$(3.5).8({\'i\':3.i,\'q\':3.q,\'x\':3.x,\'u-1e\':3.v});$(3.5+\' 7\').8({\'r\':$(3.5+\' .d-c\').i()+Z});$(3.5+\' .d-c\').8({\'q\':3.Q,\'s\':3.N,\'u-K\':t});$(3.5+\' 7 f a\').8({\'s\':3.E,\'u-K\':t,\'U\':C(3.v)+6});$(3.5+\' 7 f\').T(C(b-1)).8({\'n\':\'M\'});$(3.5+\' 7 f a\').P(9(){$(l).8({\'s\':3.L})},9(){$(l).8({\'s\':3.E})});$(3.5+\' .d-O 1x\').1w(9(e){g($(l).J(\'1v\')=="d-O-r")j(\'1z\');h j(\'o\')});g(3.I==z){k=S(9(){j(\'o\')},3.w);$(3.5).P(9(){H(k)},9(){k=S(9(){j(\'o\')},3.w)})}h{H(k)}m p=1;9 G(){p=1}9 11(){p=0}B.R(\'1A\',G);B.R(\'1i\',11);9 j(12){g(p==1){g(12=="o"){g($(3.5+\' 7 f\').14>b)b++;h b=1}h{g(b-2==-1)b=$(3.5+\' 7 f\').14;h b=b-1}g(3.y==\'10\'){$(3.5+\' 7 f\').8({\'n\':\'D\'});$(3.5+\' 7 f\').T(C(b-1)).1C()}h{$(3.5+\' 7\').1B({\'1u\':-($(3.5+\' 7 f\').U()+1t)*(b-1)})}}}$(B).1l(9(e){g($(3.5).i()<1j){$(3.5+\' .d-c\').A(\'&1n;\');$(3.5+\' .d-c\').8({\'i\':\'1o\',\'Y\':\'W 1s\'});$(3.5+\' 7\').8({\'r\':4})}h{$(3.5+\' .d-c\').A(3.c);$(3.5+\' .d-c\').8({\'i\':\'1q\',\'Y\':\'W 1p\'});$(3.5+\' 7\').8({\'r\':$(3.5+\' .d-c\').i()+Z})}})})}})(F);', 62, 103, '|||settings||modulid||ul|css|function||activenewsid|title|bn||li|if|else|width|BnAutoPlay|timername|this|var|display|next|umove|background|left|color|fontw|font|fonttextsize|timer|border|effect|true|html|window|parseInt|none|linkcolor|jQuery|startSlide|clearInterval|autoplay|attr|weight|linkhovercolor|block|titlecolor|arrows|hover|titlebgcolor|addEventListener|setInterval|eq|height|defaults|10px|isbold|padding|40|fade|stopSlide|pos|FFF|length|3000||false|brekingnews|100|id|normal|slide|bold|size|return|each|extend|blur|360|PanelUpdates|resize|EVENTS|nbsp|4px|20px|auto|fn|0px|20|marginTop|class|click|span|0078a0|prev|focus|animate|fadeIn|5aa628|333'.split('|'), 0, {}))
